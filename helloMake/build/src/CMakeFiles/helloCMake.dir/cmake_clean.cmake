08ec98b415f923dc744af79e6b502a50c2dfa4cd
Jens Franssens <jens.franssens@student.kuleuven.be>
2020-09-23 15:53:15 +0200

    added working hello world with cmake, executable helloMake/build/src/helloCMake

---
+++ helloMake/build/src/CMakeFiles/helloCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloCMake.dir/main.o"
+  "helloCMake.pdb"
+  "helloCMake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
